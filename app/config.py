e71757c1f479688ee80351df2f45823c83318f4e
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2022-09-07 18:54:41 -0500

    Moved all files to app package, setup config files with variables from local .env file

---
+++ app/config.py
@@ -0,0 +1,26 @@
+from pydantic import BaseSettings
+
+
+# .env schema:
+#
+# DATABASE_HOSTNAME=
+# DATABASE_PORT=
+# DATABASE_PASSWORD=
+# DATABASE_NAME=
+# DATABASE_USERNAME=
+# SECRET_KEY=
+# ALGORITHM=
+# ACCESS_TOKEN_EXPIRE_TIME=
+
+class Settings(BaseSettings):
+    database_hostname: str
+    database_port: str
+    database_password: str
+    database_name: str
+    database_username: str
+    secret_key: str
+    algorithm: str
+    access_token_secret: str
+
+    class Config:
+        env_file = '../.env'
