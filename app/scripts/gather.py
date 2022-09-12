c22f396f5b95f7bc724d9e0fd5e699a6a4748e25
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2022-09-12 05:42:30 -0500

    additional requirements and updated files to run in production env

--- app/scripts/gather.py
+++ app/scripts/gather.py
@@ -1,13 +1,14 @@
 import praw
-from app.config import settings
+# from app.config import settings
 import time
 import psycopg2
 from psycopg2.extras import RealDictCursor
+import os
 
 # Reddit API credentials from the .env file
-CLIENT_ID = settings.client_id
-CLIENT_SECRET = settings.client_secret
-USER_AGENT = settings.user_agent
+CLIENT_ID = os.environ.get("CLIENT_ID") # settings.client_id
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET") # settings.client_secret
+USER_AGENT = os.environ.get("USER_AGENT") # settings.user_agent
 
 
 def create_reddit_client():
@@ -19,7 +20,7 @@ def create_reddit_client():
     return client
 
 
-def collect_meme_urls() -> list[str]:
+def collect_meme_urls():
     output = []
     client = create_reddit_client()
     hot_posts = client.subreddit("memes").hot(limit=50)
@@ -48,10 +49,10 @@ if __name__ == '__main__':
     while True:
         try:
             connection = psycopg2.connect(
-                host=settings.database_hostname,
-                database=settings.database_name,
-                user=settings.database_username,
-                password=settings.database_password,
+                host=os.environ.get("DATABASE_USERNAME"), # settings.database_hostname,
+                database=os.environ.get("DATABASE_NAME"), # settings.database_name,
+                user=os.environ.get("DATABASE_USERNAME"), # settings.database_username,
+                password=os.environ.get("DATABASE_PASSWORD"), # settings.database_password,
                 cursor_factory=RealDictCursor
             )
             cursor = connection.cursor()
