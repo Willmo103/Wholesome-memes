6d2ec9cd289d875942af913b0166b6ed554ecf82
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2022-09-12 02:19:26 -0500

    Added CORS and Dockerfile

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from .routers import user, auth, meme, save
+from starlette.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 app.include_router(user.router)
 app.include_router(auth.router)
