dd375f94860fd4cf977e8b590d4082a5e7ba832c
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2022-09-28 05:17:16 -0500

    Revert "added shell scripts"
    This reverts commit 3eb175756fc9f3e1cbb1f8c15874ea7d972f6ae0.

--- app/scripts/run-gather.sh
+++
@@ -1,5 +0,0 @@
-#!
-
-source /home/will/app/venv/bin/activate
-nohup python /home/will/app/src/app/scripts/gather.py >> /home/will/app/src/app/scripts/gather.log &
-ps -fA | grep /home/will/app/src/app/scripts/gather.py >> gather-pid.log
