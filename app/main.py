3c2880d7469675ad45c5138905047d37f9399f50
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2023-01-19 20:36:36 -0600

    some pages written

--- app/main.py
+++ app/main.py
@@ -1,29 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from routers import user, auth, meme, save
 from starlette.middleware.cors import CORSMiddleware
-import os
+from sqlalchemy.orm import Session
+from .database import get_db
+import app.models as models
+from typing import Union
+from pathlib import Path
 
-gathering = False
 
 app = FastAPI()
 
-templates = Jinja2Templates("templates/")
+templates = Jinja2Templates(
+    "C:\\Users\\willm\\Documents\\GitHub\\Meme-project\\app\\templates"
+)
 
 origins = ["*"]
 
 app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(meme.router)
 app.include_router(save.router)
 
-app.get("")
+
+@app.get("/home", response_class=HTMLResponse)
+def home_page(
+    req: Request,
+    db: Session = Depends(get_db),
+    limit: int | None = 5,
+    skip: int = 0,
+):
+    # SELECT * FROM memes
+    memes = db.query(models.Meme).limit(limit).offset(skip).all()
+    # print(memes)
+    return templates.TemplateResponse("home.html", {"request": req, "memes": memes})
--- app/routers/meme.py
+++ app/routers/meme.py
@@ -1,27 +1,23 @@
 from fastapi import Response
-from .. import models, schemas
+from app import models, schemas
 from fastapi import status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
-from ..database import get_db
-from ..config import settings
+from app.database import get_db
+from app.config import settings
 from typing import Union
 
-router = APIRouter(
-    prefix="/memes",
-    tags=["memes"]
-)
+router = APIRouter(prefix="/memes", tags=["memes"])
 
 
 # Endpoint where all memes will be returned
 @router.get("/")
-def get_all_memes(db: Session = Depends(get_db), limit: Union[int | None] = 5, skip: int = 0):
+def get_all_memes(db: Session = Depends(get_db), limit: int | None = 5, skip: int = 0):
     # SELECT * FROM memes
     memes = db.query(models.Meme).limit(limit).offset(skip).all()
     # print(memes)
     if memes is None:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Nothing to return"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Nothing to return"
         )
 
     return memes
@@ -35,7 +31,7 @@ def get_memes(id: int, db: Session = Depends(get_db)):
     if meme is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Meme with id: {id} does not exist"
+            detail=f"Meme with id: {id} does not exist",
         )
 
     return meme
@@ -43,9 +39,9 @@ def get_memes(id: int, db: Session = Depends(get_db)):
 
 @router.delete("/{id}")
 def delete_meme(
-        id: int,
-        secret: schemas.AdminUser,
-        db: Session = Depends(get_db),
+    id: int,
+    secret: schemas.AdminUser,
+    db: Session = Depends(get_db),
 ):
     meme_query = db.query(models.Meme).filter(models.Meme.id == id)
     meme = meme_query.first()
@@ -53,13 +49,11 @@ def delete_meme(
     if meme is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Meme with id: {id} does not exist"
+            detail=f"Meme with id: {id} does not exist",
         )
 
     if secret.secret != settings.admin_secret:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED
-        )
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
     meme_query.delete(synchronize_session=False)
     db.commit()
